0e6eb32d86effffb2291dd9401316a10f12ab19d
Matheus Reis <matheus.reisluz@gmail.com>
2019-06-10 16:10:40 -0300

    Criação do teste E2E dos dados de VLDL Cholesterol

---
+++ features/step_definitions/widget_vldl_steps.rb
@@ -0,0 +1,18 @@
+Given('I have at least one Cholesterol-vldl registered') do
+    @expected_vldl = FactoryBot.create :vldl, :date => Time.now,
+                                                      :profile => @my_profile,
+                                                      :value => 15
+  
+    @another_vldl = FactoryBot.create :vldl, :date => Time.now,
+                                                      :profile => @my_profile,
+                                                      :value => 15
+                                                      
+    expect(@my_profile.vldls.size).to be(2)
+  end
+  
+  Then('I should see a Cholesterol-vldl widget with the most recent register') do
+    widget = find('#widget_latest_cholesterol')
+  
+    expect(widget).to have_content("Cholesterol-VLDL: #{@expected_vldl.value} mg/dl")
+    #expect(widget).to have_content("Registered: #{@expected_hdl.date.to_date()}")
+  end
