7ceadbd710ca53128152747a4758969f2037eb54
Jefferson <jeffersonss999@gmail.com>
2019-06-10 15:22:22 -0300

    Criação do teste E2E dos dados de NON-HDL Cholesterol

---
+++ features/step_definitions/widget_non_hdl_steps.rb
@@ -0,0 +1,18 @@
+Given('I have at least one Cholesterol-non-hdl registered') do
+    @expected_NON_HDL = FactoryBot.create :non_hdl, :date => Time.now,
+                                                      :profile => @my_profile,
+                                                      :value => 13
+  
+    @another_NON_HDL = FactoryBot.create :non_hdl, :date => Time.now,
+                                                      :profile => @my_profile,
+                                                      :value => 20
+                                                      
+    expect(@my_profile.non_hdls.size).to be(2)
+  end
+  
+  Then('I should see a Colesterol-non-hdl widget with the most recent register') do
+    widget = find('#widget_latest_cholesterol')
+  
+    expect(widget).to have_content("Cholesterol-NON-HDL: #{@expected_NON_HDL.value} mg/dl")
+    #expect(widget).to have_content("Registered: #{@expected_HDL.date.to_date()}")
+  end
