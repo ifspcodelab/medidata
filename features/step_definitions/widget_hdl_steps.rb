fa4b4a53f9f760d3f9cfe4969801e0f2fb0904b3
Jefferson <jeffersonss999@gmail.com>
2019-06-10 17:23:27 +0000

    Primeiro teste no widget colesterol referente aos dados de HDL-Cholesterol

---
+++ features/step_definitions/widget_hdl_steps.rb
@@ -0,0 +1,18 @@
+Given('I have at least one Cholesterol-hdl registered') do
+  @expected_hdl = FactoryBot.create :hdl, :date => Time.now,
+                                                    :profile => @my_profile,
+                                                    :value => 13
+
+  @another_hdl = FactoryBot.create :hdl, :date => Time.now,
+                                                    :profile => @my_profile,
+                                                    :value => 20
+                                                    
+  expect(@my_profile.hdls.size).to be(2)
+end
+
+Then('I should see a Colesterol-hdl widget with the most recent register') do
+  widget = find('#widget_latest_cholesterol')
+
+  expect(widget).to have_content("Cholesterol-HDL: #{@expected_hdl.value} mg/dl")
+  expect(widget).to have_content("Registered: #{@expected_hdl.date.to_date()}")
+end
